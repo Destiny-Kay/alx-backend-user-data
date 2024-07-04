b7dbb0c24692ff1967f3e516d6669b70763c5a05
Destiny-Kay <destinykevogo@gmail.com>
2024-07-04 18:46:17 +0300

    Add password hashing

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Encrypting password'''
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    '''Hashes a password'''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Validates a password'''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
