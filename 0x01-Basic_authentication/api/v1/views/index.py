5e48e6f08f8a8396bcb89658877b5e86d4b44b60
Destiny-Kay <destinykevogo@gmail.com>
2024-07-10 15:54:34 +0300

    Add few endpoints

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,19 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> str:
+    '''
+      Handles unauthorized requests
+    '''
+    return abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def not_allowed() -> str:
+    '''
+    App view to handle not allowed errors
+    '''
+    return abort(403)
