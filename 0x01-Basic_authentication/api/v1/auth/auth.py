444e648603c1198770bc01b6218b4c5e935a2c7b
Destiny-Kay <destinykevogo@gmail.com>
2024-07-10 16:19:58 +0300

    Added auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,16 @@ class Auth:
     '''Auth class template'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''checks whether urls require authentication'''
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         '''authorization_header'''
